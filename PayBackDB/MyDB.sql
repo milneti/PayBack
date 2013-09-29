a725cb58a35177da38f8cd6773655d633a0a5256
HJeon139 <gamerhj@gmail.com>
2013-09-29 18:17:19 -0400

    Fixed Server Load errors
    Note that github pretty much reformats the .mwb file and fcuks it up
    like it doesn't give a carp about the software we're building. If you
    want it, contact me or Ryan.

--- PayBackDB/MyDB.sql
+++ PayBackDB/MyDB.sql
@@ -2,149 +2,199 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+DROP SCHEMA IF EXISTS `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+SHOW WARNINGS;
 USE `mydb` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`Account`
+-- Table `Account`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Account` (
+DROP TABLE IF EXISTS `Account` ;
+
+SHOW WARNINGS;
+CREATE TABLE IF NOT EXISTS `Account` (
   `AccountID` INT(8) UNSIGNED ZEROFILL NOT NULL,
-  `Email` TINYTEXT NOT NULL,
+  `Email` VARCHAR(254) NOT NULL,
   `Password` VARCHAR(16) NOT NULL,
   `Fname` VARCHAR(16) NOT NULL,
   `Lname` VARCHAR(16) NOT NULL,
-  PRIMARY KEY (`AccountID`),
-  UNIQUE INDEX `Email_UNIQUE` (`Email` ASC),
-  UNIQUE INDEX `AccountID_UNIQUE` (`AccountID` ASC))
+  PRIMARY KEY (`AccountID`))
 ENGINE = InnoDB;
 
+SHOW WARNINGS;
+CREATE UNIQUE INDEX `Email_UNIQUE` ON `Account` (`Email` ASC);
+
+SHOW WARNINGS;
+CREATE UNIQUE INDEX `AccountID_UNIQUE` ON `Account` (`AccountID` ASC);
+
+SHOW WARNINGS;
 
 -- -----------------------------------------------------
--- Table `mydb`.`Friend_Of`
+-- Table `Friend_Of`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Friend_Of` (
+DROP TABLE IF EXISTS `Friend_Of` ;
+
+SHOW WARNINGS;
+CREATE TABLE IF NOT EXISTS `Friend_Of` (
   `UserID` INT(8) UNSIGNED ZEROFILL NOT NULL,
-  `FriendID` INT(8) UNSIGNED ZEROFILL NULL,
-  PRIMARY KEY (`UserID`),
-  INDEX `AccountID_idx` (`UserID` ASC, `FriendID` ASC),
-  CONSTRAINT `AccountID`
-    FOREIGN KEY (`UserID` , `FriendID`)
-    REFERENCES `mydb`.`Account` (`AccountID` , `AccountID`)
+  `FriendID` INT(8) UNSIGNED ZEROFILL NOT NULL,
+  CONSTRAINT `UserID`
+    FOREIGN KEY (`UserID`)
+    REFERENCES `Account` (`AccountID`)
     ON DELETE CASCADE
-    ON UPDATE CASCADE)
+    ON UPDATE CASCADE,
+  CONSTRAINT `FriendID`
+    FOREIGN KEY (`FriendID`)
+    REFERENCES `Account` (`AccountID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+SHOW WARNINGS;
+CREATE INDEX `FriendID_idx` ON `Friend_Of` (`FriendID` ASC);
+
+SHOW WARNINGS;
 
 -- -----------------------------------------------------
--- Table `mydb`.`Admin`
+-- Table `Admin`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Admin` (
+DROP TABLE IF EXISTS `Admin` ;
+
+SHOW WARNINGS;
+CREATE TABLE IF NOT EXISTS `Admin` (
   `AdminID` INT(8) UNSIGNED ZEROFILL NOT NULL,
   `Password` VARCHAR(16) NOT NULL,
   PRIMARY KEY (`AdminID`))
 ENGINE = InnoDB;
 
+SHOW WARNINGS;
 
 -- -----------------------------------------------------
--- Table `mydb`.`Transaction`
+-- Table `Transaction`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Transaction` (
+DROP TABLE IF EXISTS `Transaction` ;
+
+SHOW WARNINGS;
+CREATE TABLE IF NOT EXISTS `Transaction` (
   `TransID` INT(16) UNSIGNED ZEROFILL NOT NULL,
   `LenderID` INT(8) UNSIGNED ZEROFILL NOT NULL,
   `BorrowerID` INT(8) UNSIGNED ZEROFILL NOT NULL,
   `Amount` DECIMAL(8,2) NOT NULL,
-  `Description` TINYTEXT NULL,
+  `Description` VARCHAR(254) NULL,
   `TransDate` DATETIME NOT NULL,
   PRIMARY KEY (`TransID`),
-  INDEX `LendOrder` (`LenderID` ASC, `BorrowerID` ASC, `TransDate` ASC),
-  INDEX `BorrowerID_idx` (`BorrowerID` ASC),
   CONSTRAINT `LenderID`
     FOREIGN KEY (`LenderID`)
-    REFERENCES `mydb`.`Account` (`AccountID`)
+    REFERENCES `Account` (`AccountID`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `BorrowerID`
     FOREIGN KEY (`BorrowerID`)
-    REFERENCES `mydb`.`Account` (`AccountID`)
+    REFERENCES `Account` (`AccountID`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
+SHOW WARNINGS;
+CREATE INDEX `LendOrder` ON `Transaction` (`LenderID` ASC, `BorrowerID` ASC, `TransDate` ASC);
+
+SHOW WARNINGS;
+CREATE INDEX `BorrowerID_idx` ON `Transaction` (`BorrowerID` ASC);
+
+SHOW WARNINGS;
 
 -- -----------------------------------------------------
--- Table `mydb`.`Notification`
+-- Table `Notification`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Notification` (
+DROP TABLE IF EXISTS `Notification` ;
+
+SHOW WARNINGS;
+CREATE TABLE IF NOT EXISTS `Notification` (
   `NoteID` INT(8) UNSIGNED ZEROFILL NOT NULL,
   `SendID` INT(16) UNSIGNED ZEROFILL NOT NULL,
   `ReceiveID` INT(16) UNSIGNED ZEROFILL NOT NULL,
   `TransID` INT(16) UNSIGNED ZEROFILL NOT NULL,
-  `SendInfo` TINYTEXT NOT NULL,
+  `SendInfo` VARCHAR(254) NOT NULL,
   `NoteDate` DATETIME NOT NULL,
   PRIMARY KEY (`NoteID`),
-  INDEX `NoteOrder` (`SendID` ASC, `ReceiveID` ASC, `NoteDate` ASC),
-  INDEX `ReceiveID_idx` (`ReceiveID` ASC),
-  INDEX `TransID_idx` (`TransID` ASC),
   CONSTRAINT `SendID`
     FOREIGN KEY (`SendID`)
-    REFERENCES `mydb`.`Account` (`AccountID`)
+    REFERENCES `Account` (`AccountID`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `ReceiveID`
     FOREIGN KEY (`ReceiveID`)
-    REFERENCES `mydb`.`Account` (`AccountID`)
+    REFERENCES `Account` (`AccountID`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `TransID`
     FOREIGN KEY (`TransID`)
-    REFERENCES `mydb`.`Transaction` (`TransID`)
+    REFERENCES `Transaction` (`TransID`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
+SHOW WARNINGS;
+CREATE INDEX `NoteOrder` ON `Notification` (`SendID` ASC, `ReceiveID` ASC, `NoteDate` ASC);
+
+SHOW WARNINGS;
+CREATE INDEX `ReceiveID_idx` ON `Notification` (`ReceiveID` ASC);
+
+SHOW WARNINGS;
+CREATE INDEX `TransID_idx` ON `Notification` (`TransID` ASC);
+
+SHOW WARNINGS;
 
 -- -----------------------------------------------------
--- Table `mydb`.`NetStatus`
+-- Table `NetStatus`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`NetStatus` (
+DROP TABLE IF EXISTS `NetStatus` ;
+
+SHOW WARNINGS;
+CREATE TABLE IF NOT EXISTS `NetStatus` (
   `LogID` INT(9) UNSIGNED ZEROFILL NOT NULL,
   `LogIP` VARCHAR(45) NOT NULL,
   `LogDate` TIMESTAMP NOT NULL,
-  `UserID` INT(8) UNSIGNED ZEROFILL NOT NULL,
+  `AcctID` INT(8) UNSIGNED ZEROFILL NOT NULL,
   `MAC` VARCHAR(12) NULL,
   PRIMARY KEY (`LogID`),
-  UNIQUE INDEX `UserID_UNIQUE` (`UserID` ASC),
-  UNIQUE INDEX `MAC_UNIQUE` (`MAC` ASC),
-  CONSTRAINT `UserID`
-    FOREIGN KEY (`UserID`)
-    REFERENCES `mydb`.`Account` (`AccountID`)
+  CONSTRAINT `AcctID`
+    FOREIGN KEY (`AcctID`)
+    REFERENCES `Account` (`AccountID`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
+SHOW WARNINGS;
+CREATE UNIQUE INDEX `UserID_UNIQUE` ON `NetStatus` (`AcctID` ASC);
 
--- -----------------------------------------------------
--- Table `mydb`.`table1`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`table1` (
-  `UserID` INT(8) UNSIGNED ZEROFILL NOT NULL,
-  `MemberID` INT(8) UNSIGNED ZEROFILL NOT NULL,
-  `GroupName` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`UserID`),
-  INDEX `MemberID_idx` (`MemberID` ASC),
-  CONSTRAINT `UserID`
-    FOREIGN KEY (`UserID`)
-    REFERENCES `mydb`.`Account` (`AccountID`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `MemberID`
-    FOREIGN KEY (`MemberID`)
-    REFERENCES `mydb`.`Account` (`AccountID`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB;
+SHOW WARNINGS;
+CREATE UNIQUE INDEX `MAC_UNIQUE` ON `NetStatus` (`MAC` ASC);
 
+SHOW WARNINGS;
+SET SQL_MODE = '';
+GRANT USAGE ON *.* TO Admin;
+ DROP USER Admin;
+SET SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+SHOW WARNINGS;
+CREATE USER 'Admin' IDENTIFIED BY 'AdminPassword1';
+
+GRANT ALL ON `mydb`.* TO 'Admin';
+GRANT SELECT, INSERT, TRIGGER ON TABLE `mydb`.* TO 'Admin';
+GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE `mydb`.* TO 'Admin';
+-- GRANT EXECUTE ON ROUTINE `mydb`.* TO 'Admin';
+SHOW WARNINGS;
+SET SQL_MODE = '';
+GRANT USAGE ON *.* TO CommonUser;
+ DROP USER CommonUser;
+SET SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+SHOW WARNINGS;
+CREATE USER 'CommonUser' IDENTIFIED BY 'CommonUser12345';
+
+GRANT SELECT, INSERT, TRIGGER ON TABLE `mydb`.* TO 'CommonUser';
+GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE `mydb`.* TO 'CommonUser';
+SHOW WARNINGS;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
