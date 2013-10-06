83ecc6bf90d25c0af92eb4a226dcd46b04b4304d
milneti <tooltime4tim@yahoo.com>
2013-10-06 18:36:31 -0400

    Transaction page sequence

--- PayBack/src/com/example/payback/Transaction1Activity.java
+++ PayBack/src/com/example/payback/Transaction1Activity.java
@@ -1,12 +1,15 @@
 package com.example.payback;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 
 public class Transaction1Activity extends Activity {
+	public final static String transactionCost = "com.example.PayBack.TransCost";
+	public final static String transactionComment = "com.example.PayBack.TransComment";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +33,10 @@ public class Transaction1Activity extends Activity {
 	public void showTrans2(View view)
     {
     	Intent intent = new Intent(this, Transaction2Activity.class);
+    	EditText transCost = (EditText)findViewById(R.id.editText1);
+    	EditText transComment = (EditText)findViewById(R.id.editText2);
+    	intent.putExtra(transactionCost, transCost.getText().toString());
+    	intent.putExtra(transactionCost, transComment.getText().toString());
         startActivity(intent);
     }
 
