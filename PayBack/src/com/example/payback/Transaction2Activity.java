e6e9361b8fce96be8d305b4b6098f1c70632a1ab
astarrywonder <nova@HuyLuong.local>
2013-10-17 16:10:35 -0400

    Working on Trans 2

--- PayBack/src/com/example/payback/Transaction2Activity.java
+++ PayBack/src/com/example/payback/Transaction2Activity.java
@@ -1,22 +1,33 @@
 package com.example.payback;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class Transaction2Activity extends Activity {
+public class Transaction2Activity extends ListActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_transaction2);
 		
-		TextView costDisplay = (TextView) findViewById(R.id.textView3);
+		super.onCreate(savedInstanceState);
+	    String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+		        "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+		        "Linux", "OS/2" };
+	    // use your own layout
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				R.layout.activity_transaction2, R.id.label , values);
+	    setListAdapter(adapter);
+		
+		TextView costDisplay = (TextView) findViewById(R.id.textView2);
 		costDisplay.setText(getIntent().getStringExtra(Transaction1Activity.transactionCost));
 	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
--- PayBack/src/com/example/payback/User.java
+++ PayBack/src/com/example/payback/User.java
@@ -3,26 +3,74 @@ package com.example.payback;
 import java.util.*;
 
 public class User {
-	String fName;
-	String lName;
-	String email;
-	int userId;
+	private String fName;
+	private String lName;
+	private String email;
+	private int userId;
+	private boolean selected;	
 	static int nextIdToUse = 0; //currently it resets every time the program is run; will change with server implementation
 	ArrayList<Integer> friends; //populated whenever user inputs an email address to create a transaction with
+
 	User(String fName, String lName, String email)
 	{
-		this.fName = fName;
-		this.lName = lName;
-		this.email = email;
+		this.setfName(fName);
+		this.setlName(lName);
+		this.setEmail(email);
 		
 		//get last used userid from server
 		
-		userId = nextIdToUse++;
+		setUserId(nextIdToUse++);
 		
 		
 		//send updated last used userid back to server
 	}
+
+
+	// Getters and Setters
+
+	public String getfName() {
+		return fName;
+	}
+
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+
+	public String getlName() {
+		return lName;
+	}
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+
+	public boolean isSelected() {
+		return selected;
+	}
+
+	public void setSelected(boolean selected) {
+		this.selected = selected;
+	}
+
+	// Methods
 	static String idToEmail(int id)
 	{
 		//server stuff
@@ -34,3 +82,5 @@ public class User {
 		return -1;
 	}
 }
+
+
