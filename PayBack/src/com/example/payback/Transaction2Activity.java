84f7db1cd51367e170e65b11c9cbee54f616d0c8
astarrywonder <nova@HuyLuong.local>
2013-10-18 15:28:53 -0400

    trying to overhaul from checkbox to expandable list for future use

--- PayBack/src/com/example/payback/Transaction2Activity.java
+++ PayBack/src/com/example/payback/Transaction2Activity.java
@@ -6,34 +6,56 @@ import java.util.List;
 import android.os.Bundle;
 import android.app.ListActivity;
 import android.content.Intent;
+import android.util.SparseArray;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
 import android.widget.TextView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class Transaction2Activity extends ListActivity {
 
+	SparseArray<Transaction2Activity_expandablecontactlist_group> groups = new SparseArray<Transaction2Activity_expandablecontactlist_group>();
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		
-		super.onCreate(savedInstanceState);
-		
-		//setContentView(R.layout.activity_transaction2);
-	    View header = getLayoutInflater().inflate(R.layout.activity_transaction2_header, null);
-	    View footer = getLayoutInflater().inflate(R.layout.activity_transaction2_footer, null);
+		 super.onCreate(savedInstanceState);
+		    setContentView(R.layout.activity_transaction2_expandablecontactlist_main);
+		    createData();
+		    ExpandableListView listView = (ExpandableListView) findViewById(R.id.contactlistView);
+		    Transaction2Activity_expandablecontactlist_adapter adapter =
+		    			new Transaction2Activity_expandablecontactlist_adapter(this, groups);
+		    listView.setAdapter(adapter);
 	    
-	    // create an array of Strings, that will be put to our ListActivity
-	    ArrayAdapter<Friend> adapter = new Transaction2Activity_contactlist_adapter(this, getModel());
 	    
-	    ListView listView = getListView();
-	    listView.addHeaderView(header);
-	    listView.addFooterView(footer);
-	    
-	    setListAdapter(adapter);
-		
+//		//setContentView(R.layout.activity_transaction2);
+//	    View header = getLayoutInflater().inflate(R.layout.activity_transaction2_header, null);
+//	    View footer = getLayoutInflater().inflate(R.layout.activity_transaction2_footer, null);
+//	    
+//	    // create an array of Strings, that will be put to our ListActivity
+//	    ArrayAdapter<Friend> adapter = new Transaction2Activity_contactlist_adapter(this, getModel());
+//	    
+//	    ListView listView = getListView();
+//	    listView.addHeaderView(header);
+//	    listView.addFooterView(footer);
+//	    
+//	    setListAdapter(adapter);
+//		
 	}
+	
+	  public void createData() {
+		    for (int j = 0; j < 5; j++) {
+		    	Transaction2Activity_expandablecontactlist_group group = 
+		    			new Transaction2Activity_expandablecontactlist_group("Test " + j);
+		      for (int i = 0; i < 5; i++) {
+		        group.children.add("Sub Item" + i);
+		      }
+		      groups.append(j, group);
+		    }
+		  }
 	
 	private List<Friend> getModel() {
 	    List<Friend> list = new ArrayList<Friend>();
@@ -50,11 +72,10 @@ public class Transaction2Activity extends ListActivity {
 	    return list;
 	  }
 
-	  private Friend get(String s) {
+	private Friend get(String s) {
 	    return new Friend(s);
 	  }
 	
-
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
---
+++ PayBack/src/com/example/payback/Transaction2Activity_expandablecontactlist_group.java
@@ -0,0 +1,15 @@
+package com.example.payback;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Transaction2Activity_expandablecontactlist_group {
+
+  public String string;
+  public final List<String> children = new ArrayList<String>();
+
+  public Transaction2Activity_expandablecontactlist_group(String string) {
+    this.string = string;
+  }
+
+} 
