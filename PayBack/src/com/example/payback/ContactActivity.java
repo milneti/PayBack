589c7a99412ea02d6d188a8b95c3261f75df1b2e
astarrywonder <saltyvanilla@hotmail.com>
2013-10-25 19:53:27 -0400

    Finished debugging list in contacts. fully implemented
    dialog boxes are still broken

--- PayBack/src/com/example/payback/ContactActivity.java
+++ PayBack/src/com/example/payback/ContactActivity.java
@@ -7,6 +7,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ListActivity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
@@ -18,19 +19,17 @@ import android.widget.TextView;
 public class ContactActivity extends Activity {
 
 	  private ListView contactlistview;
+	  
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_contact);
 
-		
-
 		contactlistview = (ListView) findViewById(R.id.listofcontacts);
 		ArrayList<String> friendList = buildFriendList();
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-		        R.layout.activity_contact_rowlayout, R.id.contactlabel, friendList);
-
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.activity_contact_iteminlist,
+				friendList);
 		contactlistview.setAdapter(adapter);
 		    
 	}
@@ -40,13 +39,16 @@ public class ContactActivity extends Activity {
 	    ArrayList<String> list = new ArrayList<String>();
 	    
 	    //dummy friends
-	    Friend test1 = new Friend("Android", "Mobile");
-	    Friend test2 = new Friend("Windows7", "Windows7");
-	    Friend test3 = new Friend("iPhone", "iPhone");
 
-	    list.add(test1.getfName() + " " + test1.getlName());
-	    list.add(test2.getfName() + " " + test2.getlName());
-	    list.add(test3.getfName() + " " + test3.getlName());
+	    Friend test1 = new Friend("Android", "Mobile");
+	    Friend test2 = new Friend("Windows7", "PC");
+	    Friend test3 = new Friend("iPhone", "IOS");
+	    for(int x = 0; x < 10;x++){
+		    list.add(test1.getfName() + " " + test1.getlName());
+		    list.add(test2.getfName() + " " + test2.getlName());
+		    list.add(test3.getfName() + " " + test3.getlName());
+	    }
+
 	    return list;
 	  }
 
@@ -60,13 +62,13 @@ public class ContactActivity extends Activity {
 	
 	public void showCreateContact(View view)
     {
-		setContentView(R.layout.activity_contact);
+		//setContentView(R.layout.activity_contact);
 		LayoutInflater inflater = this.getLayoutInflater();
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
 		
-		setContentView(R.layout.dialog_create_contact);
- 	    final EditText email = (EditText)findViewById(R.id.username);		
- 	    setContentView(R.layout.activity_contact);
+		//setContentView(R.layout.dialog_create_contact);
+ 	    final EditText email = (EditText)findViewById(R.id.newemail);		
+ 	   // setContentView(R.layout.activity_contact);
  	    
  	    builder.setTitle("Add new Contact")
 		
@@ -77,13 +79,15 @@ public class ContactActivity extends Activity {
 		        	   
 		        	   //InputMethodManager inputManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE); 
 		        	   //inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),InputMethodManager.HIDE_IMPLICIT_ONLY);
-		        	   confirmContact(email.getText().toString());
 		        	   dialog.dismiss();
+		        	   confirmContact(email.getText().toString());
+		        	   
 		           }
 		       })
 		       .setNegativeButton(R.string.Cancel, new DialogInterface.OnClickListener() {
 		           public void onClick(DialogInterface dialog, int id) {
-		               dialog.dismiss();
+		              // dialog.dismiss();
+		        	   dialog.cancel();
 		           }
 		       });
 		Dialog dialog = builder.create();
@@ -93,22 +97,22 @@ public class ContactActivity extends Activity {
 	
 	public void confirmContact(final String email){
 		
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		AlertDialog.Builder builder2 = new AlertDialog.Builder(this);
 		
-		builder.setTitle("Confirm Add Contact?")
+		builder2.setTitle("Confirm Add Contact?")
 		       
 		       .setPositiveButton(R.string.Confirm, new DialogInterface.OnClickListener() {
 		           public void onClick(DialogInterface dialog, int id) {
-		        	   sendContact(email);
 		        	   dialog.dismiss();
+		        	   sendContact(email);
 		           }
 		       })
 		       .setNegativeButton(R.string.Cancel, new DialogInterface.OnClickListener() {
 		           public void onClick(DialogInterface dialog, int id) {
-		               dialog.dismiss();
+		               dialog.cancel();
 		           }
 		       });
-		Dialog dialog = builder.create();
+		Dialog dialog = builder2.create();
 		dialog.show();
 	}
 	
@@ -117,4 +121,10 @@ public class ContactActivity extends Activity {
 		TextView textView2 = (TextView)findViewById(R.id.textView2);
 		textView2.setText(email);
 	}
+	
+	public void showMainMenu(View view)
+    {
+    	Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
