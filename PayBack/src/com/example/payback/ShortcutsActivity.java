418b9cd9f4df65a9cf1be84dddc97cb096f9f5d5
Arnav Agarwal <arnavagarwal@hotmail.com>
2013-11-19 15:28:52 -0500

    Update ShortcutsActivity.java

--- PayBack/src/com/example/payback/ShortcutsActivity.java
+++ PayBack/src/com/example/payback/ShortcutsActivity.java
@@ -3,15 +3,26 @@ package com.example.payback;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.view.Menu;
 import android.view.View;
 
-public class ShortcutsActivity extends Activity {
+public class ShortcutsActivity extends Activity
+{
+	static Activity activityInstance;	//these are variables
+	static PageKillReceiver pkr;		//used for PageKillReceiver.java
+	static IntentFilter filter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_shortcuts);
+		
+		activityInstance = this;
+		pkr = new PageKillReceiver(); pkr.setActivityInstance(activityInstance);
+		filter = new IntentFilter();
+		filter.addAction("com.Payback.Logout_Intent");
+		registerReceiver(pkr, filter);
 	}
 
 	@Override
