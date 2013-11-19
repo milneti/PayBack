8c936daf76f121c0561c8c7b964c127596abe9c1
Arnav Agarwal <arnavagarwal@hotmail.com>
2013-11-19 15:28:27 -0500

    Update SettingsActivity.java

--- PayBack/src/com/example/payback/SettingsActivity.java
+++ PayBack/src/com/example/payback/SettingsActivity.java
@@ -1,14 +1,26 @@
 package com.example.payback;
 
+import android.app.Activity;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.Menu;
 
-public class SettingsActivity extends TitleActivity {
+public class SettingsActivity extends TitleActivity
+{
+	static Activity activityInstance;	//these are variables
+	static PageKillReceiver pkr;		//used for PageKillReceiver.java
+	static IntentFilter filter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		modifyTitle("Settings",R.layout.activity_settings);
+		
+		activityInstance = this;
+		pkr = new PageKillReceiver(); pkr.setActivityInstance(activityInstance);
+		filter = new IntentFilter();
+		filter.addAction("com.Payback.Logout_Intent");
+		registerReceiver(pkr, filter);
 	}
 
 	@Override
