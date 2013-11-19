0f48a859b092c6438ceadb0024b488dd239257ca
Arnav Agarwal <arnavagarwal@hotmail.com>
2013-11-19 15:29:50 -0500

    Update StatisticActivity.java

--- PayBack/src/com/example/payback/StatisticActivity.java
+++ PayBack/src/com/example/payback/StatisticActivity.java
@@ -1,14 +1,26 @@
 package com.example.payback;
 
+import android.app.Activity;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.Menu;
 
-public class StatisticActivity extends TitleActivity {
+public class StatisticActivity extends TitleActivity
+{
+	static Activity activityInstance;	//these are variables
+	static PageKillReceiver pkr;		//used for PageKillReceiver.java
+	static IntentFilter filter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		modifyTitle("Transaction Statistics",R.layout.activity_statistic);
+		
+		activityInstance = this;
+		pkr = new PageKillReceiver(); pkr.setActivityInstance(activityInstance);
+		filter = new IntentFilter();
+		filter.addAction("com.Payback.Logout_Intent");
+		registerReceiver(pkr, filter);
 	}
 
 	@Override
