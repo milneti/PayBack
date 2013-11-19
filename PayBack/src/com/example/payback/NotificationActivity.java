4d89621aa06310dfbf9f6b50f1e8e85f44ae5d35
Arnav Agarwal <arnavagarwal@hotmail.com>
2013-11-19 15:25:32 -0500

    Update NotificationActivity.java

--- PayBack/src/com/example/payback/NotificationActivity.java
+++ PayBack/src/com/example/payback/NotificationActivity.java
@@ -1,14 +1,27 @@
 package com.example.payback;
 
+import android.app.Activity;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.Menu;
 
-public class NotificationActivity extends TitleActivity {
+public class NotificationActivity extends TitleActivity
+{
 
+	static Activity activityInstance;	//these are variables
+	static PageKillReceiver pkr;		//used for PageKillReceiver.java
+	static IntentFilter filter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		modifyTitle("Notifications",R.layout.activity_notification);
+		
+		activityInstance = this;
+		pkr = new PageKillReceiver(); pkr.setActivityInstance(activityInstance);
+		filter = new IntentFilter();
+		filter.addAction("com.Payback.Logout_Intent");
+		registerReceiver(pkr, filter);
 	}
 
 	@Override
