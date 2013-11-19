79c3798b0feacb450270d37a503de177583742c3
Arnav Agarwal <arnavagarwal@hotmail.com>
2013-11-19 15:27:19 -0500

    Update ResolveTransactionActivity.java

--- PayBack/src/com/example/payback/ResolveTransactionActivity.java
+++ PayBack/src/com/example/payback/ResolveTransactionActivity.java
@@ -1,14 +1,26 @@
 package com.example.payback;
 
+import android.app.Activity;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.Menu;
 
-public class ResolveTransactionActivity extends TitleActivity {
-
+public class ResolveTransactionActivity extends TitleActivity
+{
+	static Activity activityInstance;	//these are variables
+	static PageKillReceiver pkr;		//used for PageKillReceiver.java
+	static IntentFilter filter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		modifyTitle("Resolve Transaction",R.layout.activity_resolve_transaction);
+		
+		activityInstance = this;
+		pkr = new PageKillReceiver(); pkr.setActivityInstance(activityInstance);
+		filter = new IntentFilter();
+		filter.addAction("com.Payback.Logout_Intent");
+		registerReceiver(pkr, filter);
 	}
 
 	@Override
