a857e185c1d84470a4a9071c6151262bc289c8cd
Viquoc Quan <vtquan@live.com>
2013-12-12 23:45:24 -0500

    replace dummy data with actual data

--- PayBack/src/com/example/payback/StatisticActivity.java
+++ PayBack/src/com/example/payback/StatisticActivity.java
@@ -1,6 +1,7 @@
 package com.example.payback;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.achartengine.GraphicalView;
 import org.achartengine.model.TimeSeries;
@@ -34,17 +35,6 @@ public class StatisticActivity extends TitleActivity
 		displayValue();
 
 	}
-
-    public void displayTransactionsHistory (View view)
-    {
-    	LineGraph line = new LineGraph();
-    	TimeSeries payableSeries = getPayableSeries("Payable");
-    	TimeSeries receivableSeries = getReceivableSeries("Receivable");
-    	
-    	//Open a new activity to display the graph
-    	Intent lineIntent = line.getTwoLineIntent(this, payableSeries, receivableSeries, "Transactions History");
-    	startActivity(lineIntent);
-    }    
     
     public void displayValue ()
     {   
@@ -57,7 +47,7 @@ public class StatisticActivity extends TitleActivity
     	double totalPayable = getTotalPayable(transBorrow);
     	double totalReceivable = getTotalReceivable(transLend);
     	
-    	int numTransaction = transLend.size();
+    	int numTransaction = transBorrow.size();
     	
     	
     	//Append value to textview
@@ -71,37 +61,40 @@ public class StatisticActivity extends TitleActivity
     	TotalPayableTextView.append(String.format("$%.2f", totalPayable));
     	TextView TotalReceivableTextView = (TextView) findViewById(R.id.totalReceivables);
     	TotalReceivableTextView.append(String.format("$%.2f", totalReceivable));
-//    	
-//    	//Display Pie Graph
-//    	PieGraph pie = new PieGraph();
-//    	GraphicalView gView = pie.getTwoSectionView(this, totalPayables, totalReceivables);
-//    	LinearLayout chartView = (LinearLayout) findViewById(R.id.chart);
-//    	chartView.addView(gView);
+   	
+    	if(totalPayable <= 0 && totalReceivable <= 0)
+    	{
+	    	//Display Pie Graph
+	    	PieGraph pie = new PieGraph();
+	    	GraphicalView gView = pie.getTwoSectionView(this, totalPayable, totalReceivable);
+	    	LinearLayout chartView = (LinearLayout) findViewById(R.id.chart);
+	    	chartView.addView(gView);
+    	}
     }
     
-//    public double getcurrentPayables ()
-//    {
-//     	
-//    	/*get JSON*/
-//    	JSONOBJECT jObject = new JSONObject();
-//    	JSONArray transactionJSONArray = JObject.getJSONArray("Transaction");
-//    	
-//    	/*get current payable*/    	
-//    	double currentPayable = getCurrentPayableFromJSONArray(transactionJSONArray);
-//    	
-//    	return currentPayable;
-//    }
+    public double getcurrentPayables (ArrayList<BaseTransaction> transBorrow)
+    {
+    	double currentPayable = 0;
+    	for(int i = 0; i < transBorrow.size(); i++)
+    	{
+    		if(!transBorrow.get(i).getResolved())
+    			currentPayable += transBorrow.get(i).getAmount();
+    	}
+    	
+    	return currentPayable;
+    }
     
-//    public double getcurrentReceivables ()
-//    {
-//    	double totalPayables = 0;
-//    	for(int i = 0; i < transBorrow.size(); i++)
-//    	{
-//    		totalPayables += transBorrow.get(i).getAmount();
-//    	}
-//    	
-//    	return totalPayables;
-//    }
+    public double getcurrentReceivables (ArrayList<BaseTransaction> transLend)
+    {
+    	double currentReceivable = 0;
+    	for(int i = 0; i < transLend.size(); i++)
+    	{
+    		if(!transLend.get(i).getResolved())
+    			currentReceivable += transLend.get(i).getAmount();
+    	}
+    	
+    	return currentReceivable;
+    }
     
     public double getTotalPayable (ArrayList<BaseTransaction> transBorrow)
     {
@@ -117,54 +110,37 @@ public class StatisticActivity extends TitleActivity
     public double getTotalReceivable (ArrayList<BaseTransaction> transLend)
     {
     	double totalReceivable = 0;
-    	for(int i = 0; i < transLend.size(); i++)
+    	Iterator<BaseTransaction> i1 = transLend.iterator();
+    	while(i1.hasNext())
     	{
-    		totalReceivable += transLend.get(i).getAmount();
+    		totalReceivable += i1.next().getAmount();
     	}
     	
     	return totalReceivable;
     }
+
+
+    public void displayTransactionsHistory (View view)
+    {
+    	LineGraph line = new LineGraph();
+    	TimeSeries payableSeries = getPayableSeries("Payable");
+    	TimeSeries receivableSeries = getReceivableSeries("Receivable");
+    	
+    	//Open a new activity to display the graph
+    	Intent lineIntent = line.getTwoLineIntent(this, payableSeries, receivableSeries, "Transactions History");
+    	startActivity(lineIntent);
+    }    
     
-//    public double getCurrentPayableFromJSONArray(transactionJSONArray)
-//    {
-//    	/*get array of payable*/
-//    	
-//    	/*sum up total not including those already paid off*/
-//    	
-//    	/*return value*/
-//    }
-//    
-//    public double getTotalPayableFromJSONArray(transactionJSONArray)
-//    {
-//    	/*get array of payable*/
-//    	
-//    	/*sum up total*/
-//    	
-//    	/*return value*/
-//    }
-//    
-//    public double getCurrentReceivableFromJSONArray(transactionJSONArray)
-//    {
-//    	/*get array of receivable*/
-//    	
-//    	/*sum up total not including those already paid off*/
-//    	
-//    	/*return value*/
-//    }
-//    
-//    public double getTotalReceivableFromJSONArray(transactionJSONArray)
-//    {
-//    	/*get array of receivable*/
-//    	
-//    	/*sum up total*/
-//    	
-//    	/*return value*/
-//    }
-        
     public TimeSeries getPayableSeries (String seriesName)
     {
-    	int[] x = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; // x values!
-		double[] y =  { 30, 34, 45, 57, 77, 89, 100, 111 ,123 ,145 }; // y values!
+	    ArrayList<BaseTransaction> transBorrow = user.getTransBorrow();
+
+//    	int[] x = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; // x values!
+//		double[] y =  { 30, 34, 45, 57, 77, 89, 100, 111 ,123 ,145 }; // y values!
+	    
+    	int[] x = getPayableX(transBorrow); // x values!
+		double[] y = getPayableY(transBorrow); // y values!
+		
 		TimeSeries series = new TimeSeries(seriesName); 
 		for( int i = 0; i < x.length; i++)
 		{
@@ -173,48 +149,50 @@ public class StatisticActivity extends TitleActivity
 		
 		return series;
     }
-
-/**For implementing the payable line graph*/
-//    public int[] getNumTransactionX()
-//    {
-//    	/*get JSON*/
-//    	JSONOBJECT jObject = new JSONObject();
-//    	int numTransaction = JObject.getInt("numTransaction");
-//    	
-//    	/*creating the x axis*/
-//    	int[] x = new int[numTransaction];
-//    	for(int i = 0; i < numTransaction; i++)
-//    	{
-//    		x[i] = i+1;
-//    	}
-//    }
     
-//	 public double[] getPayableY()
-//	 {
-//		/*get JSON*/
-//    	JSONOBJECT jObject = new JSONObject();
-//    	JSONArray transactionJSONArray = JObject.getJSONArray("Transaction");
-//    	int numTransaction = JObject.getInt("numTransaction");
-//		
-//    	/*get payable array*/
-//    	double[] payableArray = getPayableArray();
-//    	
-//		/*creating the y axis*/
-//    	int[] y = new int[numTransaction];    	
-//    	for(int i = 0; i < numTransaction; i++);
-//	 	{
-//	 		if(int i == 0)
-//	 		{
-//	 			y[i] = payable[1];
-//	 		}
-//	 		y[i] = i+1;
-//	 	}
-//	 }
+    public int[] getPayableX(ArrayList<BaseTransaction> transBorrow)
+    {
+    	int numTransaction = transBorrow.size();
+    	
+    	/*creating the x axis*/
+    	int[] x = new int[numTransaction];
+    	for(int i = 0; i < numTransaction; i++)
+    	{
+    		x[i] = i+1;
+    	}
+    	
+    	return x;
+    }
+    
+	public double[] getPayableY(ArrayList<BaseTransaction> transBorrow)
+	{
+    	int numTransaction = transBorrow.size();
+    	
+    	/*creating the y axis*/
+    	double[] y = new double[numTransaction];
+    	for(int i = 0; i < numTransaction; i++)
+    	{
+    		if(i == 0)
+    		{
+    			y[i] = transBorrow.get(i).getAmount();
+    		}
+    		y[i] = transBorrow.get(i).getAmount() + y[i-1];
+    	}
+    	
+    	return y;
+	}
     
 	public TimeSeries getReceivableSeries (String seriesName)
 	{
-		int[] x = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; // x values!
-		int[] y =  { 145, 123, 111, 100, 89, 77, 57, 45, 34, 30}; // y values!
+	    ArrayList<BaseTransaction> transBorrow = user.getTransBorrow();
+	    ArrayList<BaseTransaction> transLend = user.getTransLend();
+	    
+//		int[] x = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; // x values!
+//		int[] y =  { 145, 123, 111, 100, 89, 77, 57, 45, 34, 30}; // y values!
+	    
+    	int[] x = getReceivableX(transLend); // x values!
+		double[] y = getReceivableY(transLend); // y values!
+		
 		TimeSeries series = new TimeSeries(seriesName); 
 		for( int i = 0; i < x.length; i++)
 		{
@@ -222,7 +200,38 @@ public class StatisticActivity extends TitleActivity
 		}
 		
 		return series;
-		
+	}
+    
+    public int[] getReceivableX(ArrayList<BaseTransaction> transLend)
+    {
+    	int numTransaction = transLend.size();
+    	
+    	/*creating the x axis*/
+    	int[] x = new int[numTransaction];
+    	for(int i = 0; i < numTransaction; i++)
+    	{
+    		x[i] = i+1;
+    	}
+    	
+    	return x;
+    }
+    
+	public double[] getReceivableY(ArrayList<BaseTransaction> transLend)
+	{
+    	int numTransaction = transLend.size();
+    	
+    	/*creating the y axis*/
+    	double[] y = new double[numTransaction];
+    	for(int i = 0; i < numTransaction; i++)
+    	{
+    		if(i == 0)
+    		{
+    			y[i] = transLend.get(i).getAmount();
+    		}
+    		y[i] = transLend.get(i).getAmount() + y[i-1];
+    	}
+    	
+    	return y;
 	}
 	
     public void showMainMenu(View view) {
